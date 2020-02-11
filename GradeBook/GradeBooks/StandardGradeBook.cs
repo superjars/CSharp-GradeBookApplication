3278700db6bab9a261a0541023d7614a3a3f04d9
superjars <superjars@gmail.com>
2020-02-10 22:09:49 -0600

    Completed module

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -9,7 +9,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
-            this.Type = GradeBookType.Standard
+            Type = GradeBookType.Standard;
         }
     }
 }
